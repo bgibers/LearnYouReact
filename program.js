58d9b134f07eec6445592423189e79f912a8909a
Brendan Giberson <bgibers@clemson.edu>
2019-08-13 22:33:33 -0400

    Load props from server complete

--- program.js
+++ program.js
@@ -1,19 +1,24 @@
-var express = require('express');
-    var app = express();
+var express = require('express')
+var app = express()
 
-    app.set('port', (process.argv[2] || 3000));
-    app.set('view engine', 'jsx');
-    app.set('views', __dirname + '/views');
-    app.engine('jsx', require('express-react-views').createEngine({ transformViews: false }));
+app.set('port', (process.argv[2] || 3000))
+app.set('view engine', 'jsx')
+app.set('views', __dirname + '/views')
+app.engine('jsx', require('express-react-views').createEngine({ transformViews: false }))
 
-    require('babel/register')({
-        ignore: false
-    });
+require('babel/register')({
+  ignore: false
+})
 
-    app.use('/', function(req, res) {
-      res.render('index', '');
-    });
+var data = [
+  { title: 'Shopping', detail: process.argv[3] },
+  { title: 'Hair cut', detail: process.argv[4] }
+]
 
-    app.listen(app.get('port'), function() {
-        console.log('Express server is up on port 3000');
-    });
+app.use('/', function (req, res) {
+  res.render('index', { data: data })
+})
+
+app.listen(app.get('port'), function () {
+  console.log('Express server is up on port 3000')
+})
